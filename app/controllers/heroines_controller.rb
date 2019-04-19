332b302fb106ee7791712976a832236f4ade951a
Elliot <lltchen1@gmail.com>
2019-04-19 12:13:04 -0400

    crushed it

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,27 @@
 class HeroinesController < ApplicationController
+
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+    @power = @heroine.power
+  end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.create(
+      params.require(:heroine).permit(:super_name, :name, :power_id))
+      redirect_to heroines_path
+  end
+
+
+
+
+
 end
